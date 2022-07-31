78c7c97561ceb143d2f0f9eac7de9b23ab7de1a4
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-07-31 18:26:50 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Sarah Ibrahim';
+//const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
